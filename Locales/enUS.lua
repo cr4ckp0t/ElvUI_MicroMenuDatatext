c144891a3bb981885e2d1f2f9bd73f03a229b736
Adam Koch <me@adamko.ch>
2020-01-19 00:46:20 -0500

    Updated localizations.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,25 +1,23 @@
 -------------------------------------------------------------------------------
--- ElvUI_MicroMenuDatatext By Crackpotx (US, Lightbringer)
+-- ElvUI MicroMenuDatatext By Crackpotx (US, Lightbringer)
 -------------------------------------------------------------------------------
-local debug = false
---@debug@
-debug = true
---@end-debug@
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("ElvUI_MicroMenuDatatext", "enUS", true, debug)
+local L = AceLocale:NewLocale("ElvUI_MicroMenuDatatext", "enUS", true, true)
 if not L then return end
 
-L["<Click> to open the micro menu."] = true
 L["Default Click Action"] = true
 L["Default action when you left click on the datatext."] = true
 L["ElvUI Chat Tweaks"] = true
 L["ElvUI Config"] = true
+L["ElvUI"] = true
 L["Game Menu"] = true
-L["Crackpotx"] = true
-L["Micro Menu Datatext by"] = true
+L["Left Click"] = true
+L["Micro Menu Datatext By Crackpotx"] = true
 L["Micro Menu Datatext"] = true
 L["Micro Menu"] = true
+L["Open Micro Menu"] = true
 L["Plugins by |cff9382c9Crackpotx|r"] = true
+L["Right Click"] = true
 L["Show ElvUI CT"] = true
 L["Show ElvUI Config"] = true
 L["Show a shortcut to open ElvUI Chat Tweaks' config in the menu.\n\n|cffff0000This only applies if this addon is loaded.|r"] = true
@@ -29,9 +27,11 @@ L["Thanks for using and supporting my work!  -- |cff9382c9Crackpotx|r\n\n|cffff0
 L["Toggle Achievements"] = true
 L["Toggle Character Frame"] = true
 L["Toggle Collections Frame"] = true
+L["Toggle Collections Journal"] = true
 L["Toggle Dungeons Frame"] = true
 L["Toggle ElvUI Chat Tweaks Config"] = true
-L["Toggle ElvUI Config"] = true
+L["Toggle ElvUI Options UI"] = true
+L["Toggle Encounter Journal"] = true
 L["Toggle Encounters Guide"] = true
 L["Toggle Game Menu"] = true
 L["Toggle Guild Frame"] = true
