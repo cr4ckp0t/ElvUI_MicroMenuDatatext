bb677a3d7704fbcb571584246622e1b8d22ac71a
Adam Koch <me@adamko.ch>
2020-02-16 13:54:55 -0500

    Localization improvements.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -5,36 +5,4 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_MicroMenuDatatext", "enUS", true, true)
 if not L then return end
 
-L["Default Click Action"] = true
-L["Default action when you left click on the datatext."] = true
-L["ElvUI Chat Tweaks"] = true
-L["ElvUI Config"] = true
-L["ElvUI"] = true
-L["Game Menu"] = true
-L["Left Click"] = true
-L["Micro Menu Datatext By Crackpotx"] = true
-L["Micro Menu Datatext"] = true
-L["Micro Menu"] = true
-L["Open Micro Menu"] = true
-L["Plugins by |cff9382c9Crackpotx|r"] = true
-L["Right Click"] = true
-L["Show ElvUI CT"] = true
-L["Show ElvUI Config"] = true
-L["Show a shortcut to open ElvUI Chat Tweaks' config in the menu.\n\n|cffff0000This only applies if this addon is loaded.|r"] = true
-L["Show a shortcut to open ElvUI's config in the menu."] = true
-L["Thanks for using and supporting my work!  - |cff9382c9Crackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
-L["Thanks for using and supporting my work!  -- |cff9382c9Crackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
-L["Toggle Achievements"] = true
-L["Toggle Character Frame"] = true
-L["Toggle Collections Frame"] = true
-L["Toggle Collections Journal"] = true
-L["Toggle Dungeons Frame"] = true
-L["Toggle ElvUI Chat Tweaks Config"] = true
-L["Toggle ElvUI Options UI"] = true
-L["Toggle Encounter Journal"] = true
-L["Toggle Encounters Guide"] = true
-L["Toggle Game Menu"] = true
-L["Toggle Guild Frame"] = true
-L["Toggle Quest Log"] = true
-L["Toggle Spellbook"] = true
-L["Toggle Talents"] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
